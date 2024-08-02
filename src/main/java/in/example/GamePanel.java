ac22a27e644830087a5b94d07e5d24c450377af9
leo2005310 <107912599+leo2005310@users.noreply.github.com>
2024-08-02 21:38:10 +0800

    Add files via upload

---
+++ src/main/java/in/example/GamePanel.java
@@ -0,0 +1,11 @@
+package in.example;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+    public GamePanel() {
+        setSize(680,580);
+        setBackground(Color.BLACK);
+    }
+}
